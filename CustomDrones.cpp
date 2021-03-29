f4a7955352ee6f60768d24a845d7ab16d6847331
mathchamp93 <81532572+mathchamp93@users.noreply.github.com>
2021-03-28 22:11:48 -0400

    Update CustomDrones.cpp
    Partial fix for boarder drone AI. Disables the custom AI for boarding drones. Seems to fix boarding drones getting stuck and ion intruders sitting in one room. However, ion intruders now (still?) stop to "fight" crewmembers when in empty rooms.

--- CustomDrones.cpp
+++ CustomDrones.cpp
@@ -162,7 +162,7 @@ HOOK_METHOD(ShipManager, CommandCrewMoveRoom, (CrewMember* crew, int room) -> bo
     if (blockControllableAI)
     {
         auto custom = CustomCrewManager::GetInstance();
-        if (crew->GetControllable() || (custom->IsRace(crew->species) && custom->GetDefinition(crew->species).droneAI.hasCustomAI)) return false;
+        if (crew->GetControllable() || (!crew->intruder && custom->IsRace(crew->species) && custom->GetDefinition(crew->species).droneAI.hasCustomAI)) return false;
     }
 
     return super(crew, room);
@@ -173,7 +173,7 @@ HOOK_METHOD(CrewMember, SetTask, (CrewTask task) -> void)
     if (blockControllableAI)
     {
         auto custom = CustomCrewManager::GetInstance();
-        if (GetControllable() || (custom->IsRace(species) && custom->GetDefinition(species).droneAI.hasCustomAI)) return;
+        if (GetControllable() || (!intruder && custom->IsRace(species) && custom->GetDefinition(species).droneAI.hasCustomAI)) return;
     }
 
     super(task);
@@ -202,7 +202,7 @@ HOOK_METHOD(CrewAI, UpdateDrones, () -> void)
 
     for (auto crew : crewList)
     {
-        if (crew->IsDrone() && custom->IsRace(crew->species))
+        if (crew->IsDrone() && !crew->intruder && custom->IsRace(crew->species))
         {
             auto def = custom->GetDefinition(crew->species);
             if (def.droneAI.hasCustomAI && !crew->GetControllable() && !crew->IsDead() && crew->Functional() && crew->crewAnim->status != 3)
