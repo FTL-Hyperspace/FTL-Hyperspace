3ad8dc6eee9414b51a86cbc40e95cc7bc65faaf8
TheSwiftTiger <chapagainsafal@hotmail.com>
2021-04-16 19:14:22 +0100

    Fixed weird system/augment blueprints

--- CustomStore.cpp
+++ CustomStore.cpp
@@ -29,11 +29,6 @@ static ItemPrice ParsePriceNode(rapidxml::xml_node<char>* node)
                 }
             }
 
-            if (cNode->first_attribute("flat"))
-            {
-                def.flatModifier = EventsParser::ParseBoolean(cNode->first_attribute("flat")->value());
-            }
-
             if (!val.empty())
             {
                 def.price = boost::lexical_cast<int>(val);
@@ -56,9 +51,9 @@ static ItemPrice ParsePriceNode(rapidxml::xml_node<char>* node)
                 }
             }
 
-            if (cNode->first_attribute("flat") && EventsParser::ParseBoolean(cNode->first_attribute("flat")->value()) == true)
+            if (cNode->first_attribute("flat"))
             {
-                def.flatModifier = true;
+                def.flatModifier = EventsParser::ParseBoolean(cNode->first_attribute("flat")->value());
             }
 
             if (!val.empty())
@@ -106,8 +101,8 @@ static ResourceItem ParseResourceNode(rapidxml::xml_node<char>* node)
         {
             if (cNode->first_attribute("min") && cNode->first_attribute("max"))
             {
-                def.minMaxCount.first = boost::lexical_cast<int>(cNode->first_attribute("min"));
-                def.minMaxCount.second = boost::lexical_cast<int>(cNode->first_attribute("max"));
+                def.minMaxCount.first = boost::lexical_cast<int>(cNode->first_attribute("min")->value());
+                def.minMaxCount.second = boost::lexical_cast<int>(cNode->first_attribute("max")->value());
             }
 
             if (!val.empty())
@@ -432,8 +427,6 @@ std::vector<StoreBox*> StoreComplete::CreateCustomStoreBoxes(const StoreCategory
                     box->desc.cost = GetItemPricing(i.price, box->blueprint->desc.cost, orig->worldLevel);
 
                     vec.push_back(box);
-
-                    delete bp;
                 }
             }
         }
@@ -509,8 +502,6 @@ std::vector<StoreBox*> StoreComplete::CreateCustomStoreBoxes(const StoreCategory
                     box->desc.cost = GetItemPricing(i.price, box->desc.cost, orig->worldLevel);
 
                     vec.push_back(box);
-
-                    delete bp;
                 }
 
             }
@@ -956,7 +947,6 @@ void StoreComplete::MouseMove(int x, int y)
                 for (auto i : sec.storeBoxes[sec.currentSection])
                 {
                     i->orig->MouseMove(x, y);
-
 
                     if ((i->orig->button.bHover || i->orig->button.bSelected) && i->orig->count > 0)
                     {
