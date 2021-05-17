a767ea016733559580f42729ff26def00b75f02d
TheSwiftTiger <chapagainsafal@hotmail.com>
2021-05-17 18:52:44 +0100

    For changing engines to have a pseudorandom probability distribution

---
+++ PseudorandomEngines.cpp
@@ -0,0 +1,18 @@
+#include "Global.h"
+
+static float CFromP(float p)
+{
+    return 0.f;
+}
+
+static float PFromC(float c)
+{
+    return 0.f;
+}
+
+static float GetEngineProbability(int counter, float evasion)
+{
+    return 0.f;
+}
+
+
