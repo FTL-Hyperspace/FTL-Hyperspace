4e17491c299b37949be783ff581fec29cd9efc2e
laszlogasd <laszlo.gasd@gmail.com>
2021-09-30 23:35:10 +0200

    First pre-draft

--- CustomizedSystems.h
+++ CustomizedSystems.h
@@ -1,1 +1,26 @@
 #include "Global.h
+#include <array>
+
+
+
+
+
+
+struct customizedSystem
+{
+    customizedSystem(int startVal, int overValSize, int valInc, int startCost, int overCostSize, int costInc): valueIncrement(valInc), costIncrement(costInc) {
+
+    }
+
+    customizedSystem(int startCost, int overCostSize, int costInc): costIncrement(costInc) {
+
+    }
+
+    int valueIncrement = 0;
+    vector<int> valueOverride;
+
+    int costIncrement = 0;
+    vector<int> costOverride;
+
+};
+
