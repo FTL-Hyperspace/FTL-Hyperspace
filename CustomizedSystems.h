5417a1ce2bd73a1005e170c6e657b2170199aa2c
laszlogasd <laszlo.gasd@gmail.com>
2021-10-22 00:29:12 +0200

    [Sync]

--- CustomizedSystems.h
+++ CustomizedSystems.h
@@ -9,11 +9,15 @@
 struct customizedSystem
 {
     customizedSystem(int startVal, int overValSize, int valInc, int startCost, int overCostSize, int costInc): valueIncrement(valInc), costIncrement(costInc) {
-
+        valueOverride.pushback(startVal);
+        valueOverride.resize(overValSize + 1, -1);
+        costOverride.pushback(startCost);
+        costOverride.resize(overCostSize + 1, -1);
     }
 
     customizedSystem(int startCost, int overCostSize, int costInc): costIncrement(costInc) {
-
+        costOverride.pushback(startCost);
+        costOverride.resize(overCostSize + 1, -1);
     }
 
     int valueIncrement = 0;
@@ -21,6 +25,11 @@ struct customizedSystem
 
     int costIncrement = 0;
     vector<int> costOverride;
+
+};
+
+struct allCustomizedSystems
+{
 
 };
 
