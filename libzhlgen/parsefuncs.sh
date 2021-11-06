7c4af8f8e5232ea7e3fb2463a0a4c3050011f9cf
Mr. Doom <mrdoom@dulake.net>
2021-11-05 17:34:54 -0700

    Parsefuncs.sh can now be run from any path and still will run the Lua scripts correctly

--- libzhlgen/parsefuncs.sh
+++ libzhlgen/parsefuncs.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 
-cd ./lua
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR/lua
 lua parsefuncs.lua ../test/FTLGameStripped.h ../test/functions/win32/1.6.9 ../../FTLGame.cpp ../../FTLGame.h
