624e043fbdf3d654368b69db5cc188dee56c405c
mathchamp93 <81532572+mathchamp93@users.noreply.github.com>
2022-02-06 12:48:44 -0500

    Fixed weapon/drone override hang

--- CustomShipGenerator.cpp
+++ CustomShipGenerator.cpp
@@ -497,7 +497,7 @@ void CustomShipGenerator::AddOverrideWeapons(ShipManager *ship, ShipEvent& event
 		weaponOverride.erase(weaponOverride.begin()+idx);
 	}
 	int maxWeaponPower = ship->GetSystemPowerMax(SYS_WEAPONS);
-	while (maxWeaponPower < weaponPower)
+	if (maxWeaponPower < weaponPower)
 	{
 		for (int i=0; i < weaponPower-maxWeaponPower; ++i)
 		{
@@ -533,7 +533,7 @@ void CustomShipGenerator::AddOverrideDrones(ShipManager *ship, ShipEvent& event)
 		droneOverride.erase(droneOverride.begin()+idx);
 	}
 	int maxDronePower = ship->GetSystemPowerMax(SYS_DRONES);
-	while (maxDronePower < dronePower)
+	if (maxDronePower < dronePower)
 	{
 		for (int i=0; i < dronePower-maxDronePower; ++i)
 		{
@@ -836,7 +836,7 @@ HOOK_STATIC_PRIORITY(ShipGenerator, CreateShip, 9999, (const std::string& name, 
 		weaponOverride.erase(weaponOverride.begin()+idx);
 	}
 	int maxWeaponPower = ship->GetSystemPowerMax(SYS_WEAPONS);
-	while (maxWeaponPower < weaponPower)
+	if (maxWeaponPower < weaponPower)
 	{
 		for (int i=0; i < weaponPower-maxWeaponPower; ++i)
 		{
@@ -868,7 +868,7 @@ HOOK_STATIC_PRIORITY(ShipGenerator, CreateShip, 9999, (const std::string& name, 
 		droneOverride.erase(droneOverride.begin()+idx);
 	}
 	int maxDronePower = ship->GetSystemPowerMax(SYS_DRONES);
-	while (maxDronePower < dronePower)
+	if (maxDronePower < dronePower)
 	{
 		for (int i=0; i < dronePower-maxDronePower; ++i)
 		{
