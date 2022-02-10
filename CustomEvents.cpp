d07bb4b2453e0d59ed5b27e0be1d2f4f29d57207
mathchamp93 <81532572+mathchamp93@users.noreply.github.com>
2022-02-09 19:21:04 -0500

    Fix issue with weapons updating with zero time step

--- CustomEvents.cpp
+++ CustomEvents.cpp
@@ -5191,6 +5191,11 @@ HOOK_METHOD(WorldManager, CheckStatusEffects, (std::vector<StatusEffect> &effect
     cFPS->SpeedFactor = speed;
 }
 
+HOOK_METHOD(ProjectileFactory, Update, () -> void)
+{
+    if (G_->GetCFPS()->SpeedFactor > 0.f) super();
+}
+
 // Vanilla method has a bug so let's rewrite it
 HOOK_METHOD_PRIORITY(WorldManager, CheckStatusEffects, 9999, (std::vector<StatusEffect> &effects) -> void)
 {
