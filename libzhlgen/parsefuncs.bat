97f04889f8904d16d9b61ffbb1fd39dec1cc167d
mathchamp93 <81532572+mathchamp93@users.noreply.github.com>
2022-02-16 21:26:10 -0500

    Updated windows parsefuncs scripts

--- libzhlgen/parsefuncs.bat
+++ libzhlgen/parsefuncs.bat
@@ -3,6 +3,7 @@ set inputPath=%~dpnx1
 set funcPath=%~dpnx2
 set outputCPP=%~dpnx3
 set outputH=%~dpnx4
+set platform=%5
 pushd %~dp0\lua
-lua parsefuncs.lua "%inputPath%" "%funcPath%" "%outputCPP%" "%outputH%"
+lua parsefuncs.lua "%inputPath%" "%funcPath%" "%outputCPP%" "%outputH%" %platform%
 popd
--- libzhlgen/test/generate.bat
+++ libzhlgen/test/generate.bat
@@ -1,3 +1,4 @@
 :: parsefuncs <input file> <function definitions path> <output cpp> <output h>
-..\parsefuncs FTLGameStripped.h functions ..\..\FTLGame.cpp ..\..\FTLGame.h
+CALL ..\parsefuncs functions\win32\FTLGameStripped.h functions\win32\1.6.9 ..\..\FTLGameWin32.cpp ..\..\FTLGameWin32.h i386-w64-mingw32
+CALL ..\parsefuncs functions\ELF_x86\FTLGameStripped.ELF32.h functions\ELF_x86\1.6.13 ..\..\FTLGameELF32.cpp ..\..\FTLGameELF32.h i386-pc-linux
 pause
