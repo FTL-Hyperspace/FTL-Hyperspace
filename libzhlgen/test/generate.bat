9ea0a26077b8096c880f1bf11024d8714a379c14
mathchamp93 <81532572+mathchamp93@users.noreply.github.com>
2022-04-01 18:28:57 -0400

    Added ELF64 to windows generate script

--- libzhlgen/test/generate.bat
+++ libzhlgen/test/generate.bat
@@ -1,4 +1,5 @@
 :: parsefuncs <input file> <function definitions path> <output cpp> <output h>
 CALL ..\parsefuncs functions\win32\FTLGameStripped.h functions\win32\1.6.9 ..\..\FTLGameWin32.cpp ..\..\FTLGameWin32.h i386-w64-mingw32
 CALL ..\parsefuncs functions\ELF_x86\FTLGameStripped.ELF32.h functions\ELF_x86\1.6.13 ..\..\FTLGameELF32.cpp ..\..\FTLGameELF32.h i386-pc-linux
+CALL ..\parsefuncs functions\ELF_amd64/FTLGameStripped.h functions\ELF_amd64\1.6.13 ..\..\FTLGameELF64.cpp ..\..\FTLGameELF64.h x86_64-pc-linux
 pause
