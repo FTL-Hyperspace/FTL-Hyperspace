59a88b4433c3084de1fbd0dcfe79ef5b46ac365f
Mr. Doom <1423894+Nasa62@users.noreply.github.com>
2022-05-04 12:52:21 -0700

    Disable Lua package library for now, we'll manually implemeant a require later that can't load DLLs

--- linit.c
+++ linit.c
@@ -38,7 +38,7 @@
 */
 static const luaL_Reg loadedlibs[] = {
   {"_G", luaopen_base},
-  {LUA_LOADLIBNAME, luaopen_package},
+//  {LUA_LOADLIBNAME, luaopen_package},
 //  {LUA_COLIBNAME, luaopen_coroutine},
   {LUA_TABLIBNAME, luaopen_table},
 //  {LUA_IOLIBNAME, luaopen_io},
