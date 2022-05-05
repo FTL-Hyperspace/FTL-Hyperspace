f26120bd6e4b0aa86c9705c992e1c587ab7c6112
Mr. Doom <1423894+Nasa62@users.noreply.github.com>
2022-05-05 12:17:33 -0700

    Testing adding a function to Lua with doxygen

--- LuaScriptInit.cpp
+++ LuaScriptInit.cpp
@@ -1,7 +1,33 @@
 #include "LuaScriptInit.h"
 #include <string>
+#include <assert.h>
+#include <boost/algorithm/string.hpp>
+#include <boost/format.hpp>
+#include "Global.h"
 
-std::string luaTest = "print(\"hello\")\nfunction helloWorld()\n\tprint(\"Hello World\")\nend\nos.exit()\n";
+/*
+    TODO Stuff
+    - Have a `global_run` (or `global`) table that contents is saved with the run (functions not allowed to be inserted into the table, data only)
+    - Have a `profile` (or `global_profile`) table that contents is saved across runs (functions not allowed, data only)
+    - Add serpent library to allow easy printing of Lua tables with `serpent.block()` for debugging for developers of Lua scripts
+    - Add a print function to write messages to the in-game screen like a chat/console
+    - Add console LUA execution (single strings)
+    - Implement table_size() like Factorio does for tables with non-contiguous keys.?
+    - Maybe add abilitly to pull localized string?
+    - Re-implement require to load only .lua files from the dat, or maybe re-enable lua package (which contains require) and figure out how to restrict it.
+    - Re-implement math.random to use FTL's random.
+    - Add script table
+    - Add script.on_init( (function callback/registration) for functions to run on a new run
+    - Add script.on_load( (function callback) for functions to run upon loading the script, this is before the game is loaded or a run is started
+    - Add script.on_event(eventEnum, (function callback)) for functions to run upon a defined event (you cannot stop the event but hopefully we can make the code wait for your code to finish and it not being async, maybe we can add an on_async_event in the future)
+    - Add defines table
+    - Add defines.events table
+    - Add defines.events.on_tick and some other events.
+    - Maybe add a story system like https://wiki.factorio.com/Tutorial:Scripting#Story_script ?
+    
+*/
+
+std::string luaTest = "print(\"hello\")\nfunction helloWorld()\n\tprint(\"Hello World\")\nend\nlog(\"LUA MESSAGE\")\n";
 
 void removeDangerousStuff(lua_State* lua)
 {
@@ -32,7 +58,46 @@ When called without arguments, returns a pseudo-random float with uniform distri
 
 This function is an interface to the underling pseudo-random generator function provided by C.
 */
+ 
+ /** @defgroup luaglobal LuaGlobals
+  */
+ 
+ /** General Global Lua Script functions
+  * @ingroup luaglobal
+  */
+ class Script
+ {
+ }
 
+/** @addtogroup luaglobal
+ *
+ * @{
+ */
+ 
+/** Do logging
+ */
+auto hs_lua_log2 (lua_State* lua) -> int {}
+
+#if DOXYGEN_ONLY
+/** Log a message to the Hyperspace log file & standard out
+ * @param msg Message string you would like to log
+ */
+static void log(char* msg) {}
+#endif // DOXYGEN_ONLY
+
+static int printMessage(lua_State* lua)
+{
+    assert(lua_isstring(lua, 1));
+    const char* msg = lua_tostring(lua, 1);
+    const char* logMessage = boost::str(boost::format("[Lua]: %s\n") % msg).c_str();
+    printf(logMessage);
+    hs_log_file(logMessage);
+    return 0;
+}
+/** @} */
+
+/** Initialize Lua Scripting
+ */
 LuaScriptInit::LuaScriptInit()
 {
     printf("Initializing Lua\n");
@@ -41,6 +106,9 @@ LuaScriptInit::LuaScriptInit()
     luaL_openlibs(lua); // Load Lua libraries (we restricted what libraries can load in linit.c)
     removeDangerousStuff(lua);
     // TODO: Figure out how to unload and/or replace certain library functions (like math.random!, threads! & other things unsafe for a single-threaded deterministic environment)
+    
+    lua_pushcfunction(lua, printMessage);
+    lua_setglobal(lua, "log");
 
     printf("Loading Lua string\n");
     int iErr = 0;
