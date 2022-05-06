1f8938a99166606735201290259dc733f5314363
Mr. Doom <1423894+Nasa62@users.noreply.github.com>
2022-05-06 12:15:09 -0700

    More docs

--- lua/LuaLibScript.h
+++ lua/LuaLibScript.h
@@ -27,9 +27,28 @@ class LuaLibScript
          *
          * Your function will be called once upon loading the game (not upon starting a new run use @{on_init} for that)
          *
+         * Functions will be called in the order they are registered, lua entry scripts are executed in the order they are defined
+         * in the hyperspace.xml, so if your mod was defined before another in Slipstream it'll load before the other mod.
+         *
          * **Warning:** you can accidentally register your function multiple times and it will be called multiple times!
          *
          * Actually currently runs on `AcheivementTracker::LoadAchievementDescriptions` (right after most of Hyperspace initalizes XML stuff)
+         *
+         * @usage 
+         * function myModInitializationCode()
+         *   log("My code was run after the game started")
+         *   if not _G["myModLoaded"] then
+         *     _G["myModLoaded"] = true
+         *   end
+         *   -- Assuming "OtherMod" defines a global "otherModLoaded" value just like how
+         *   -- "MyMod" declared a "myModLoaded" here, we can see if their code has already run.
+         *   -- This is not a HS feature, it's up to other mods to do something like this.
+         *   if not _G["otherModLoaded"] then
+         *     warningWindow("Warning! MyMod depends on OtherMod!")
+         *   end
+         * end
+         *
+         * script.on_load(myModInitializationCode)
          */
         static int l_on_load(lua_State* lua);
         
@@ -38,6 +57,10 @@ class LuaLibScript
          * @tparam function callback Callback function to register
          *
          * Your function will be called once upon starting a new run (and/or loading a run)
+         *
+         *
+         * Functions will be called in the order they are registered, lua entry scripts are executed in the order they are defined
+         * in the hyperspace.xml, so if your mod was defined before another in Slipstream it'll load before the other mod.
          *
          * **Warning:** you can accidentally register your function multiple times and it will be called multiple times!
          *
