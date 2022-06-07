1d3a8e7921d4602702a5908c70720325f9807224
nedsociety <nedsociety@users.noreply.github.com>
2022-06-07 21:01:42 +0900

    Windows: mingw-g++ 9.0, no recompilation (probably using SEH), compiles but not running

--- .devcontainer/triplets/x86-windows-ftl.cmake
+++ .devcontainer/triplets/x86-windows-ftl.cmake
@@ -2,6 +2,6 @@ set(VCPKG_TARGET_ARCHITECTURE x86)
 set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 
-set(VCPKG_CMAKE_SYSTEM_NAME Windows)
+set(VCPKG_CMAKE_SYSTEM_NAME MinGW)
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "/vcpkg/scripts/toolchains/ftl-windows.cmake")
 
