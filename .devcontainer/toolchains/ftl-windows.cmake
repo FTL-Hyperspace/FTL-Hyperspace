13729e6c6a57ca47ca30a28cbbf2203afd34cdb5
nedsociety <nedsociety@users.noreply.github.com>
2022-06-09 14:10:46 +0900

    Switching to clang: successfully builds deps, compiles sources but link fails with error related to exception handling

--- .devcontainer/toolchains/ftl-windows.cmake
+++ .devcontainer/toolchains/ftl-windows.cmake
@@ -1,24 +1,42 @@
 set(CMAKE_SYSTEM_NAME Windows)
 
-# Info taken from `i686-w64-mingw32-g++ -v`
-string(CONCAT _compiler_flags ""
-#"-target i686-pc-windows-gnu -m32"
-# " -I/usr/i686-w64-mingw32/include"
-# " -Bi686-w64-mingw32-"
+string(CONCAT _compiler_flags
+"-target i686-w64-mingw32 -nostdinc"
+
+# clang++-10 -E -x c -v - < /dev/null
+# This should override xmmintrin.h so that gcc intrinsics for vector instructions are not used
+" -isystem /usr/lib/llvm-10/lib/clang/10.0.0/include"
+# Force mingw to use clang intrinsics
+# REF: https://github.com/msys2/MINGW-packages/issues/9052#issuecomment-756456305
+" -D__MINGW_FORCE_SYS_INTRINS"
+
+# i686-w64-mingw32-g++-posix -E -x c++ - -v < /dev/null
+" -isystem /usr/lib/gcc/i686-w64-mingw32/9.3-posix/include/c++"
+" -isystem /usr/lib/gcc/i686-w64-mingw32/9.3-posix/include/c++/i686-w64-mingw32"
+" -isystem /usr/lib/gcc/i686-w64-mingw32/9.3-posix/include/c++/backward"
+" -isystem /usr/lib/gcc/i686-w64-mingw32/9.3-posix/include"
+# Do not include fixincludes
+# REF: https://codechecker.readthedocs.io/en/v6.9.0/gcc_incompatibilities/
+#" -isystem /usr/lib/gcc/i686-w64-mingw32/9.3-posix/include-fixed"
+" -isystem /usr/i686-w64-mingw32/include"
+
+# Use utils starting with `i686-w64-mingw32-`
+" -Bi686-w64-mingw32-"
+
+# Use DWARF exceptions
+" -fdwarf-exceptions"
 )
 
-set(CMAKE_C_COMPILER "/usr/bin/i686-w64-mingw32-gcc-posix" CACHE PATH "")
-set(CMAKE_CXX_COMPILER "/usr/bin/i686-w64-mingw32-g++-posix" CACHE PATH "")
+set(CMAKE_C_COMPILER "/usr/bin/clang-10" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/bin/clang++-10" CACHE PATH "")
 set(CMAKE_C_FLAGS_INIT "${_compiler_flags}")
 set(CMAKE_CXX_FLAGS_INIT "${_compiler_flags}")
 set(CMAKE_C_FLAGS_DEBUG_INIT "-DDEBUG")
 set(CMAKE_CXX_FLAGS_DEBUG_INIT "-DDEBUG")
 set(CMAKE_C_FLAGS_RELEASE_INIT "-DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELEASE_INIT "-DNDEBUG")
-# set(CMAKE_SHARED_LINKER_FLAGS_INIT "-static -L/usr/lib/gcc/i686-w64-mingw32/9.3-posix")
-# set(CMAKE_EXE_LINKER_FLAGS_INIT "-static -L/usr/lib/gcc/i686-w64-mingw32/9.3-posix")
-set(CMAKE_SHARED_LINKER_FLAGS_INIT "-static")
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-static")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-static -L/usr/i686-w64-mingw32/lib -L/usr/lib/gcc/i686-w64-mingw32/9.3-posix")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-static -L/usr/i686-w64-mingw32/lib -L/usr/lib/gcc/i686-w64-mingw32/9.3-posix")
 
 # From mingw-w64-i686-dev package
 set(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
--- .devcontainer/triplets/x86-windows-ftl.cmake
+++ .devcontainer/triplets/x86-windows-ftl.cmake
@@ -3,5 +3,8 @@ set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME MinGW)
+# Workaround for clang-created .lib files: "Could not find proper second linker member"
+# REF: https://github.com/microsoft/vcpkg/issues/24095#issuecomment-1096705410
+set(VCPKG_POLICY_SKIP_ARCHITECTURE_CHECK enabled)
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "/vcpkg/scripts/toolchains/ftl-windows.cmake")
 
