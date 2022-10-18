1a764fe3f9e7a9f47c6821862b057cd7da593257
nedsociety <nedsociety@users.noreply.github.com>
2022-10-18 13:19:58 +0900

    ci: fix lowercase issue

--- buildall-from-docker.sh
+++ buildall-from-docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-docker run -it --rm -v $SCRIPT_DIR:/ftl ghcr.io/FTL-Hyperspace/hs-devcontainer bash -c "cd /ftl && ./buildall.sh"
+docker run -it --rm -v $SCRIPT_DIR:/ftl ghcr.io/ftl-hyperspace/hs-devcontainer bash -c "cd /ftl && ./buildall.sh"
