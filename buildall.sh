baf51b439b246d44fc78136de284c25d4ca2ff78
ChronoVortex <nick@hudsucker.com>
2023-02-12 15:09:08 -0800

    undid changes to buildall

--- buildall.sh
+++ buildall.sh
@@ -18,4 +18,10 @@ function build_one_variant() {
     ninja -C $BUILD_DIR
 }
 
+build_one_variant build-linux-1.6.13-debug amd64-linux-ftl ON Debug
+build_one_variant build-linux-1.6.12-debug amd64-linux-ftl OFF Debug
+build_one_variant build-windows-debug x86-windows-ftl OFF Debug
+
+build_one_variant build-linux-1.6.13-release amd64-linux-ftl ON Release
+build_one_variant build-linux-1.6.12-release amd64-linux-ftl OFF Release
 build_one_variant build-windows-release x86-windows-ftl OFF Release
