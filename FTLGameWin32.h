f609aa0fed4867e28e2a0fa6294a8d1284e3946a
Vertaalfout <104238697+Vertaalfout@users.noreply.github.com>
2023-07-30 02:11:22 -0400

    Fixed ion bug (Issue #132)
    Fixed bug where when a system had more than 9 ion damage applied, it crashed the game due to rendering an invalid primitive off the end of the timerCircle array

--- FTLGameWin32.h
+++ FTLGameWin32.h
@@ -1261,6 +1261,7 @@ struct LIBZHL_INTERFACE ShipSystem
 	LIBZHL_API bool DecreasePower(bool force);
 	LIBZHL_API int GetEffectivePower();
 	LIBZHL_API static std::string __stdcall GetLevelDescription(int systemId, int level, bool tooltip);
+	LIBZHL_API TimerHelper GetLockTimer();
 	LIBZHL_API bool GetLocked();
 	LIBZHL_API int GetMaxPower();
 	LIBZHL_API int GetPowerCap();
@@ -1272,6 +1273,8 @@ struct LIBZHL_INTERFACE ShipSystem
 	LIBZHL_API void LockSystem(int lock);
 	LIBZHL_API static int __stdcall NameToSystemId(const std::string &name);
 	LIBZHL_API int RenderPowerBoxes(int x, int y, int width, int height, int gap, int heightMod, bool flash);
+	LIBZHL_API static int __stdcall RenderPowerBoxesPlain(int x, int y, int width, int height, int gap, int current, int temp, int max);
+	LIBZHL_API void RenderSystemSymbol(bool forPowerUI, int forceColor);
 	LIBZHL_API void SaveState(int file);
 	LIBZHL_API void SetPowerCap(int cap);
 	LIBZHL_API int SetPowerLoss(int power);
