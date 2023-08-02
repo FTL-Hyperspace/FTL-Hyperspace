543036e1da62545b5254d086d74f624eaec89e9e
Vertaalfout <104238697+Vertaalfout@users.noreply.github.com>
2023-08-02 01:47:05 +0000

    New print function for lua (#149)
    * New print function for lua
    
    * Added lua formatting to new print function
    
    * Removed [Lua] prefix from PrintHelper arg and added varargs support for log command

--- lua/luaGlobal.h
+++ lua/luaGlobal.h
@@ -7,6 +7,7 @@
 #include <assert.h>
 #include <boost/algorithm/string.hpp>
 #include <boost/format.hpp>
+#include "../CommandConsole.h"
 
 /*** Functions available outside a module
  * @module global
@@ -34,10 +35,54 @@
  */
 static int hslua_g_printMessage(lua_State* lua)
 {
-    const char* msg = luaL_checkstring(lua, 1);
-    std::string logMessage = boost::str(boost::format("[Lua]: %s\n") % msg);
-    printf("%s", logMessage.c_str());
-    hs_log_file("%s", logMessage.c_str());
+    std::string message = std::string();
+    int argCount = lua_gettop(lua);
+    lua_getglobal(lua, "tostring");
+    for (int i = 1; i <= argCount; i++)
+    {
+        lua_pushvalue(lua, -1);
+        lua_pushvalue(lua, i);
+        lua_call(lua, 1, 1);
+        const char* str = lua_tostring(lua, -1);
+        if (str == NULL)
+        {
+            return luaL_error(lua, "'tostring' must return a string to 'log'");
+        }
+        
+        message += str;
+        message += "    ";
+        lua_pop(lua, 1);
+    }
+    message = boost::str(boost::format("[Lua]: %s\n") % message);
+    printf("%s", message.c_str());
+    hs_log_file("%s", message.c_str());
+    return 0;
+}
+
+static int hslua_g_printToScreen(lua_State* lua)
+{
+    std::string message = std::string();
+    int argCount = lua_gettop(lua);
+    lua_getglobal(lua, "tostring");
+    for (int i = 1; i <= argCount; i++)
+    {
+        lua_pushvalue(lua, -1);
+        lua_pushvalue(lua, i);
+        lua_call(lua, 1, 1);
+        const char* str = lua_tostring(lua, -1);
+        if (str == NULL)
+        {
+            return luaL_error(lua, "'tostring' must return a string to 'print'");
+        }
+        
+        message += str;
+        message += "    ";
+        lua_pop(lua, 1);
+    }
+    PrintHelper::GetInstance()->AddMessage(message);
+    message = boost::str(boost::format("[Lua]: %s\n") % message);
+    printf("%s", message.c_str());
+    hs_log_file("%s", message.c_str());
     return 0;
 }
 
@@ -46,6 +91,7 @@ static int hslua_g_printMessage(lua_State* lua)
 void hsluaglobal_register(lua_State* lua)
 {
     lua_register(lua, "log", hslua_g_printMessage);
+    lua_register(lua, "print", hslua_g_printToScreen);
 }
 
 #endif // LUAGLOBAL_H_INCLUDED
