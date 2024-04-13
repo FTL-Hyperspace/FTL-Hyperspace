b3f3ec44916b469c862b049b41d174e13581b055
ChronoVortex <nick@hudsucker.com>
2024-04-13 01:00:01 -0700

    fix player drone surge crashing the game if restarting a run after destroying a ship

--- FTLGameELF64.h
+++ FTLGameELF64.h
@@ -6720,6 +6720,7 @@ struct Ship : ShipObject
 	
 	LIBZHL_API void BreachRandomHull(int roomId);
 	LIBZHL_API void BreachSpecificHull(int grid_x, int grid_y);
+	LIBZHL_API bool DestroyedDone();
 	LIBZHL_API int EmptySlots(int roomId);
 	LIBZHL_API bool FullRoom(int roomId, bool intruder);
 	LIBZHL_API int GetAvailableRoom(int preferred, bool intruder);
