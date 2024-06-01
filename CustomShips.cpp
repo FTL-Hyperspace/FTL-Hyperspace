3a2c900f7555fa39ef2fc988ba630a56c81fbba7
gabrielsubzero <gabrieldossantoscooper@gmail.com>
2024-06-01 17:53:08 -0300

    Fix?

--- CustomShips.cpp
+++ CustomShips.cpp
@@ -932,9 +932,9 @@ HOOK_METHOD(ShipManager, DamageSystem, (int roomId, Damage dmg) -> void)
     super(roomId, dmg);
 }
 
-HOOK_METHOD(ShipManager, DamageArea, (Pointf location, Damage dmg, bool forceHit) -> void)
+HOOK_METHOD(ShipManager, DamageHull, (int dmg, bool force) -> void)
 {
-    LOG_HOOK("HOOK_METHOD -> ShipManager::DamageArea -> Begin (CustomDamage.cpp)\n")
+    LOG_HOOK("HOOK_METHOD -> ShipManager::DamageHull -> Begin (CustomDamage.cpp)\n")
 
     int roomId = ship.GetSelectedRoomId(location.x, location.y, true);
     auto ex = RM_EX(ship.vRoomList[roomId]);
@@ -953,34 +953,6 @@ HOOK_METHOD(ShipManager, DamageArea, (Pointf location, Damage dmg, bool forceHit
     }
 
     super(dmg);
-}
-
-HOOK_METHOD(ShipManager, DamageBeam, (Pointf location1, Pointf location2, Damage dmg) -> void)
-{
-    LOG_HOOK("HOOK_METHOD -> ShipManager::DamageBeam -> Begin (CustomDamage.cpp)\n")
-
-    int room1 = ship.GetSelectedRoomId(location1.x, location1.y, true);
-    int room2 = ship.GetSelectedRoomId(location2.x, location2.y, true);
-    int willDamage = room1 != room2
-    if (willDamage)
-    {
-        auto ex = RM_EX(ship.vRoomList[room2]);
-
-        if (random32() % 100 < ex->hullDamageResistChance && dmg.iDamage > 0)
-        {
-            dmg.iSystemDamage = +dmg.iDamage;
-            dmg.iPersDamage = +dmg.iDamage;
-            dmg.iDamage = 0;
-            auto msg = new DamageMessage(1.f, ship.GetRoomCenter(roomId), DamageMessage::MessageType::RESIST);
-            msg->color.r = 255.f / 255.f;
-            msg->color.g = 0.f / 255.f;
-            msg->color.b = 255.f / 255.f;
-            msg->color.a = 1.f;
-            damMessages.push_back(msg);
-        }
-
-        super(dmg);
-    }
 }
 
 HOOK_METHOD(ShipAI, SetStalemate, (bool stalemate) -> void)
