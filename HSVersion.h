89c448a6ab08c8861bbab80d660a712943f6b881
ChronoVortex <33610911+ChronoVortex@users.noreply.github.com>
2024-08-03 12:48:22 -0700

    update version

--- HSVersion.h
+++ HSVersion.h
@@ -6,7 +6,7 @@
 Change the version numbers here
 */
 #define HS_VER_MAJOR 1
-#define HS_VER_MINOR 12
+#define HS_VER_MINOR 13
 #define HS_VER_PATCH 0
 
 #define BUILD_IDENTIFIER_HASH "unknown_build"
