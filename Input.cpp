6ed6aa423d5073f144205ad4e597b5f93ca0536e
The Dumb Dino <Ppan71749@gmail.com>
2024-08-15 17:11:27 +0200

    Added Controller input detection
    - Input detection is functioning for the default controller buttons, d-pads, and joysticks.
    - It appears that not all values are operational. In particular, joystick and d-pad x/y values are not functioning as intended. Button functionality remains intact.
    - I recommend using live view in a terminal when reviewing the logs yourself.

--- Input.cpp
+++ Input.cpp
@@ -31,6 +31,7 @@ static void OnScrollWheel(float direction)
 HOOK_METHOD(CEvent, OnEvent, (const InputEvent* inputEvent) -> void)
 {
     LOG_HOOK("HOOK_METHOD -> CEvent::OnEvent -> Begin (Input.cpp)\n")
+
     if (inputEvent->type == InputEventType::INPUT_EVENT_MOUSE)
     {
         MouseInputEvent *mEvent = (MouseInputEvent*)(&inputEvent->event);
@@ -40,6 +41,51 @@ HOOK_METHOD(CEvent, OnEvent, (const InputEvent* inputEvent) -> void)
             OnScrollWheel(mEvent->scroll);
         }
     }
+    if (inputEvent->type == InputEventType::INPUT_EVENT_JOYSTICK)
+    {
+        JoystickInputEvent *jEvent = (JoystickInputEvent*)(&inputEvent->event);
+
+        if (jEvent->index != 0 || jEvent->index != 1)
+        {
+            hs_log_file("JOYSTICK EVENT:\n Device: %d - Index: %d - x-pos: %d - y-pos: %d \n", 
+                        jEvent->device, jEvent->index, jEvent->x, jEvent->y);
+        }
+    }
+
+    switch (inputEvent->detail)
+    {
+    case INPUT_JOYSTICK_CONNECTED:
+        hs_log_file("\nController Connected \n");
+        break;
+    case INPUT_JOYSTICK_DISCONNECTED:
+        hs_log_file("\nController Disconnected \n");
+        break;
+    case INPUT_JOYSTICK_BUTTON_DOWN:
+        hs_log_file("\nController Button Pressed \n");
+        break;
+    case INPUT_JOYSTICK_BUTTON_UP:
+        hs_log_file("\nController Button Released \n");
+        break;
+    case INPUT_JOYSTICK_DPAD_CHANGE:
+        hs_log_file("\nController DPAD Interacted \n");
+        break;
+    case INPUT_JOYSTICK_STICK_CHANGE:
+        hs_log_file("\nController Joystick Interacted \n");
+        break;
+    default:
+        hs_log_file("\nNon-Controller Event: %d\n", inputEvent->detail);
+        break;
+    }
 
     super(inputEvent);
 }
+
+/*
+struct InputEvent
+{
+  InputEventType type;
+  InputEventDetail detail;
+  double timestamp;
+  InputEventUnion event;
+};
+*/
