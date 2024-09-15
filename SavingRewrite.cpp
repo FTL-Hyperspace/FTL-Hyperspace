a0c6893cc1ab217a4d35e761b18b044c268f1502
The Dumb Dino <Ppan71749@gmail.com>
2024-09-15 14:58:09 +0200

    Reverse engineered Animation saving
    - Reverse engineered both the load and save function of the Animation struct

--- SavingRewrite.cpp
+++ SavingRewrite.cpp
@@ -1,9 +1,53 @@
 #include "Global.h"
 
+HOOK_METHOD_PRIORITY(Animation, LoadState, 9999, (int fd) -> void)
+{
+    LOG_HOOK("HOOK_METHOD_PRIORITY -> Animation::LoadState -> Begin (SavingRewrite.cpp)\n")
+    
+    // Reverse engineered Vanilla code by Dino
+    if (FileHelper::readInteger(fd) != 0)
+    {
+        Start(true);
+    }
+    if (FileHelper::readInteger(fd) != 0)
+    {
+        tracker.loop = true;
+        tracker.loopDelay = 0.0;
+        tracker.currentDelay = 0.0;
+    }
+    currentFrame = FileHelper::readInteger(fd);
+    tracker.current_time = FileHelper::readFloat(fd) * tracker.time;
+    fScale = FileHelper::readFloat(fd);
+    position.x = FileHelper::readFloat(fd);
+    position.y = FileHelper::readFloat(fd);
+    // End of orig code
+}
+
+HOOK_METHOD_PRIORITY(Animation, SaveState, 9999, (int fd) -> void)
+{
+    LOG_HOOK("HOOK_METHOD_PRIORITY -> Animation::SaveState -> Begin (SavingRewrite.cpp)\n")
+    
+    // Reverse engineered Vanilla code by Dino
+    FileHelper::writeInt(fd, tracker.running);
+    FileHelper::writeInt(fd, tracker.loop);
+    FileHelper::writeInt(fd, currentFrame);
+    float val = 0.0;
+    if ((tracker.running) && (val = tracker.current_time / tracker.time, val <= 0.0))
+    {
+        val = 0.0;
+    }
+    FileHelper::writeFloat(fd, val);
+    FileHelper::writeFloat(fd, fScale);
+    FileHelper::writeFloat(fd, position.x);
+    FileHelper::writeFloat(fd, position.y);
+    // End of orig code
+}
+
 HOOK_METHOD_PRIORITY(BoarderPodDrone, LoadState, 9999, (int fd) -> void)
 {
     LOG_HOOK("HOOK_METHOD_PRIORITY -> BoarderPodDrone::LoadState -> Begin (SavingRewrite.cpp)\n")
-        
+
+    // Reverse engineered Vanilla code by Dino
     SpaceDrone::LoadState(fd);
     // BoarderPodDrone Values
     diedInSpace = FileHelper::readInteger(fd) != 0;
@@ -21,7 +65,7 @@ HOOK_METHOD_PRIORITY(BoarderPodDrone, LoadState, 9999, (int fd) -> void)
     if (bDeliveredDrone)
     {
         BoarderDrone *drone = boarderDrone;
-        // drone->droneRoom = roomId; doesn't need to be asigned, enter drones are never on own ship
+        // drone->droneRoom = roomId; vanilla code asigns fake value, enter drones are never on own ship - I skip this entirely
         drone->SetRoom(roomId);
         drone->SetPosition(Point(posX, posY));
         drone->MoveToRoom(roomId, slotId, true);
@@ -33,12 +77,14 @@ HOOK_METHOD_PRIORITY(BoarderPodDrone, LoadState, 9999, (int fd) -> void)
             SetInstantPowered();
         }
     }
+    // End of orig code
 }
 
 HOOK_METHOD_PRIORITY(BoarderPodDrone, SaveState, 9999, (int fd) -> void)
 {
     LOG_HOOK("HOOK_METHOD_PRIORITY -> BoarderPodDrone::SaveState -> Begin (SavingRewrite.cpp)\n")
-    
+
+    // Reverse engineered Vanilla code by Dino
     SpaceDrone::SaveState(fd);
     FileHelper::writeInt(fd, diedInSpace);
     FileHelper::writeInt(fd, bDeliveredDrone);
@@ -49,4 +95,5 @@ HOOK_METHOD_PRIORITY(BoarderPodDrone, SaveState, 9999, (int fd) -> void)
     FileHelper::writeInt(fd, boarderDrone->GetLocation().y);
     FileHelper::writeInt(fd, boarderDrone->currentSlot.roomId);
     FileHelper::writeInt(fd, boarderDrone->currentSlot.slotId);
+    // End of orig code
 }
