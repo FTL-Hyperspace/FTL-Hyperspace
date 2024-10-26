c85a801d1df20a6dc8360d90a85b4af145d4db72
THETASOLA <36167048+THETASOLA@users.noreply.github.com>
2024-10-26 03:45:05 +0200

    Custom sound for achievements (#380)

--- CustomAchievements.h
+++ CustomAchievements.h
@@ -7,14 +7,16 @@ struct CustomAchievement
     CAchievement ach;
 
     TextString name;
-	TextString description;
+    TextString description;
 
-	TextString secretName;
-	TextString secretDescription;
+    TextString secretName;
+    TextString secretDescription;
 
-	bool hidden = false;
+    std::string sound;
 
-	void ParseAchievement(rapidxml::xml_node<char> *node, std::string *varName=nullptr);
+    bool hidden = false;
+
+    void ParseAchievement(rapidxml::xml_node<char> *node, std::string *varName=nullptr);
 };
 
 class CustomAchievementSection
