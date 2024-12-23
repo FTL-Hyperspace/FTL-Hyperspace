ca976377516e3492b53517806f524770e649a442
The Dumb Dino <77470472+The-Dumb-Dino@users.noreply.github.com>
2024-12-23 16:56:11 +0100

    Implementing building for Mac
    - Added 2 new folder in the buildscripts/ directory for 1.6.12 & 13.
    - Added a new triplet file "amd64-dawrin-ftl.cmake" but it is not recognised by docker rn.
    - Also added an incomplete "amd64-dawrin-ftl.cmake" toolchain file which needs to be adjusted so that it can be built.
    - Updated .gitignore to ignore Clion specific paths and files

--- buildscripts/darwin-1.6.13/build-releaseonly.sh
+++ buildscripts/darwin-1.6.13/build-releaseonly.sh
@@ -3,4 +3,4 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-$SCRIPT_DIR/../build-one-variant.sh build-linux-1.6.13-debug amd64-linux-ftl ON Debug
+$SCRIPT_DIR/../build-one-variant.sh build-darwin-1.6.13-release amd64-darwin-ftl ON Release
