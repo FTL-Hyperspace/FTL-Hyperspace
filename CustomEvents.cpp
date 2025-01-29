2175538e3f865dc5ab3fddc782238a1f0fcac28c
Pepson <36167048+THETASOLA@users.noreply.github.com>
2025-01-29 11:33:29 +0100

    Adding current event debt to calculation of the cost requirements of following event (#415)
    * handling case where two costing event back to back would only check for half of the cost requirements
    
    * add a nice comment
    
    ---------
    
    Co-authored-by: ranhai613 <85445781+ranhai613@users.noreply.github.com>
    Co-authored-by: ranhai613 <hulanhai613@gmail.com>

--- CustomEvents.cpp
+++ CustomEvents.cpp
@@ -6290,6 +6290,24 @@ HOOK_METHOD(WorldManager, CheckRequirements, (LocationEvent *event, bool hidden)
 
     if (ret && event)
     {
+        // fix a vanilla bug that player can select a choice that requires more resources than they have when two costing events are back to back
+        // this is because requirements check is done before the cost of the previous event is applied to the resources balance
+        int fuel_cost = event->stuff.fuel + lastLocationEvent->stuff.fuel;
+        if (fuel_cost < 0 && playerShip->shipManager->fuel_count < fuel_cost * -1)
+            return false;
+
+        int missiles_cost = event->stuff.missiles + lastLocationEvent->stuff.missiles;
+        if (missiles_cost < 0 && playerShip->shipManager->GetMissileCount() < missiles_cost * -1)
+            return false;
+
+        int drones_cost = event->stuff.drones + lastLocationEvent->stuff.drones;
+        if (drones_cost < 0 && playerShip->shipManager->GetDroneCount() < drones_cost * -1)
+            return false;
+
+        int scrap_cost = event->stuff.scrap + lastLocationEvent->stuff.scrap;
+        if (scrap_cost < 0 && playerShip->shipManager->currentScrap < scrap_cost * -1)
+            return false;
+
         CustomEvent *customEvent = CustomEventsParser::GetInstance()->GetCustomEvent(event->eventName);
 
         if (customEvent)
