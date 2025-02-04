d4c941f00d00208db2b47996f9bacb6321ba4c9c
sillysandvich <teamfortress.stupid@gmail.com>
2025-02-04 22:50:13 +0000

    User can enable jump map wait menu (issue #439) (#442)
    * User can enabled jump map wait menu (issue #439)
    
    * Add SetValid linux sig
    
    * wiki tweak

--- FTLGameWin32.h
+++ FTLGameWin32.h
@@ -1882,6 +1882,8 @@ struct CSurface
 
 struct LIBZHL_INTERFACE GenericButton
 {
+    inline bool Hovering() {return bActive && bHover;}
+
 	virtual ~GenericButton() {}
 	virtual void Reset() LIBZHL_PLACEHOLDER
 	LIBZHL_API virtual void SetLocation(Point pos);
@@ -6296,6 +6298,7 @@ struct MouseControl
 	LIBZHL_API void SetDoor(int state);
 	LIBZHL_API void SetTooltip(const std::string &tooltip);
 	LIBZHL_API void SetTooltipTitle(const std::string &tooltip);
+	LIBZHL_API void SetValid(bool valid, bool newValid);
 	
 	Point position;
 	Point lastPosition;
