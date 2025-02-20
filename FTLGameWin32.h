7d193c2b22e62af76d85759ee33f8a8108c8007e
Pepson <36167048+THETASOLA@users.noreply.github.com>
2025-02-20 17:17:11 +0100

    updating VTable_SystemBox on every OS

--- FTLGameWin32.h
+++ FTLGameWin32.h
@@ -1095,8 +1095,8 @@ struct LIBZHL_INTERFACE SystemBox
 		this->constructor(pos, sys, playerUI);
 	}
 
-	virtual ~SystemBox() {}
 	virtual void destroy() LIBZHL_PLACEHOLDER
+	virtual ~SystemBox() {}
 	virtual bool HasButton() LIBZHL_PLACEHOLDER
 	virtual int GetCooldownBarHeight() LIBZHL_PLACEHOLDER
 	virtual int GetHeightModifier() LIBZHL_PLACEHOLDER
