5e9bc35767a02612f0067caa9c36bbbea5dd2ae4
sillysandvich <teamfortress.stupid@gmail.com>
2025-02-24 09:35:49 +0000

    Fix ship select buttons' behavior between vanilla and custom (#468)
    * Address issue #436
    
    Ship selection random button now selects from all available ships.
    
    * change the behavior of CustomShipSelect::CountUnlockedShips(-1) and fix some conditions
    
    * first page integration
    
    * Fix incomplete push
    
    * adjust ScoreKeeper's selector and add some tweak

--- FTLGameWin32.h
+++ FTLGameWin32.h
@@ -6736,6 +6736,7 @@ struct TopScore
 
 struct ScoreKeeper
 {
+    int CountUnlockedShips(int variant);
 
 	LIBZHL_API void AddScrapCollected(int scrap);
 	LIBZHL_API int AddTopScoreList(TopScore &score, std::vector<TopScore> &topScoreList);
