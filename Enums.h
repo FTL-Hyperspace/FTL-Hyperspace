5eac510013f457f98691ef83bf070f95f48e3c23
ranhai613 <85445781+ranhai613@users.noreply.github.com>
2025-04-15 16:33:38 +0900

    merge Environments.h into Enums.h

--- Enums.h
+++ Enums.h
@@ -67,3 +67,17 @@ enum TaskId
     TASK_KILLING = 9,
     TASK_SABOTAGING = 10
 };
+
+enum EnvId
+{
+    ENV_NONE = 0,
+    ENV_ASTEROIDS = 1,
+    ENV_SUN = 2,
+    ENV_NEBULA = 3,
+    ENV_STORM = 4,
+    ENV_PULSAR = 5,
+    ENV_PDS = 6,
+    ENV_AVOIDED = 8,
+    ENV_PDS_FRIENDLY = 9,
+    ENV_TOTAL = 10
+};
