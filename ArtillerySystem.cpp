9860a4bf07163095f31d2f0315106e7418186011
ranhai613 <85445781+ranhai613@users.noreply.github.com>
2025-05-11 23:48:36 +0900

    halt non-hostile enemy's artillery firing

--- ArtillerySystem.cpp
+++ ArtillerySystem.cpp
@@ -226,3 +226,21 @@ HOOK_METHOD(WeaponControl, SetAutofiring, (bool on, bool simple) -> void)
         }
     }
 }
+
+// Prevent non-hostile enemy's artillery from firing when the system power is provided by energy crews (i.g. zoltans)
+static bool g_haltArtilleryFire = false;
+HOOK_METHOD(ArtillerySystem, OnLoop, () -> void)
+{
+    LOG_HOOK("HOOK_METHOD -> ArtillerySystem::OnLoop -> Begin (ArtillerySystem.cpp)\n")
+    if (projectileFactory->targetId == 1 || !G_->GetShipManager(1) || G_->GetShipManager(1)->_targetable.hostile) return super();
+
+    g_haltArtilleryFire = true;
+    super();
+    g_haltArtilleryFire = false;
+}
+HOOK_METHOD(ProjectileFactory, ReadyToFire, () -> bool)
+{
+    LOG_HOOK("HOOK_METHOD -> ProjectileFactory::ReadyToFire -> Begin (ArtillerySystem.cpp)\n")
+    if (g_haltArtilleryFire) return false;
+    return super();
+}
