55f0d96a4746e4ac6fc67110070a41264321437a
ChronoVortex <33610911+ChronoVortex@users.noreply.github.com>
2026-03-25 14:19:40 -0700

    update devcontainer

--- .devcontainer/devcontainer.sh
+++ .devcontainer/devcontainer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Devcontainer configuration
-export DEVCONTAINER_IMAGE="ghcr.io/fr-eed/hs-devcontainer"
-export DEVCONTAINER_TAG="latest"
+export DEVCONTAINER_IMAGE="ghcr.io/ftl-hyperspace/hs-devcontainer"
+export DEVCONTAINER_TAG="v2"
 export DEVCONTAINER_FULL="${DEVCONTAINER_IMAGE}:${DEVCONTAINER_TAG}"
