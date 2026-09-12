8de8e2fe1660e5ca3e344619e35467cf64cf71fa
ATARAXIA MECHANICA <rockpaperkatana@gmail.com>
2026-09-12 13:56:35 +0800

    Fix scrambled drones not spinning

--- CustomDrones.cpp
+++ CustomDrones.cpp
@@ -783,7 +783,7 @@ HOOK_METHOD(DefenseDrone, PickTarget, () -> void)
                 if (aimingAngle > 360.f) aimingAngle -= 360.f;
 
                 bool bNewSwivelDir = (aimingAngle <= desiredAimingAngle || (aimingAngle - desiredAimingAngle) >= 180.f) && (desiredAimingAngle < aimingAngle || (desiredAimingAngle - aimingAngle) < 180.f);
-                if (bNewSwivelDir != bSwivelDir) //if swiveled too much and went past, snap to angle
+                if (bNewSwivelDir != bSwivelDir) // if swiveled too much and went past, snap to angle
                 {
                     aimingAngle = desiredAimingAngle;
                 }
@@ -849,48 +849,39 @@ HOOK_METHOD(DefenseDrone, PickTarget, () -> void)
             return;
         }
     }
-    else
+    else //scrambled
     {
-        if (desiredAimingAngle == aimingAngle || desiredAimingAngle < 0.f)
+        if (desiredAimingAngle == aimingAngle) desiredAimingAngle = random32() % 360; // if arrived at angle, plot a new random one
+
+        if (*(int*)(&targetLocation.x) == 0xff7fffff || *(int*)(&targetLocation.y) == 0xff7fffff)
         {
-            desiredAimingAngle = random32() % 360;
-            if (*(int*)(&targetLocation.x) == 0xff7fffff || *(int*)(&targetLocation.y) == 0xff7fffff || desiredAimingAngle >= 0.f)
+            float speedFactor = G_->GetCFPS()->GetSpeedFactor();
+
+            float swivelSpeed;
+            float swivelDirection;
+            bool bSwivelDir;
+
+            if ((aimingAngle <= desiredAimingAngle || (aimingAngle - desiredAimingAngle) >= 180.f) && (desiredAimingAngle < aimingAngle || (desiredAimingAngle - aimingAngle) < 180.f))
             {
-                float speedFactor = G_->GetCFPS()->GetSpeedFactor();
+                swivelSpeed = 30.f;
+                swivelDirection = 1.f;
+                bSwivelDir = true;
+            }
+            else
+            {
+                swivelSpeed = -30.f;
+                swivelDirection = -1.f;
+                bSwivelDir = false;
+            }
 
-                if (desiredAimingAngle <= 0.f)
-                {
-                    Pointf relPos = Pointf(targetLocation.x - currentLocation.x, targetLocation.y - currentLocation.y);
-                    desiredAimingAngle = atan2f(relPos.y, relPos.x) * 180.f / 3.141592654f;
-                    if (desiredAimingAngle < 0.f) desiredAimingAngle += 360.f;
-                }
+            aimingAngle += swivelSpeed * speedFactor;
+            if (aimingAngle < 0.f) aimingAngle += 360.f;
+            if (aimingAngle > 360.f) aimingAngle -= 360.f;
 
-                float swivelSpeed;
-                float swivelDirection;
-                bool bSwivelDir;
-
-                if ((aimingAngle <= desiredAimingAngle || (aimingAngle - desiredAimingAngle) >= 180.f) && (desiredAimingAngle < aimingAngle || (desiredAimingAngle - aimingAngle) < 180.f))
-                {
-                    swivelSpeed = 30.f;
-                    swivelDirection = 1.f;
-                    bSwivelDir = true;
-                }
-                else
-                {
-                    swivelSpeed = -30.f;
-                    swivelDirection = -1.f;
-                    bSwivelDir = false;
-                }
-
-                aimingAngle += swivelSpeed * speedFactor;
-                if (aimingAngle < 0.f) aimingAngle += 360.f;
-                if (aimingAngle > 360.f) aimingAngle -= 360.f;
-
-                bool bNewSwivelDir = (aimingAngle <= desiredAimingAngle || (aimingAngle - desiredAimingAngle) >= 180.f) && (desiredAimingAngle < aimingAngle || (desiredAimingAngle - aimingAngle) < 180.f);
-                if (bNewSwivelDir != bSwivelDir) //if swiveled too much and went past, snap to angle
-                {
-                    aimingAngle = desiredAimingAngle;
-                }
+            bool bNewSwivelDir = (aimingAngle <= desiredAimingAngle || (aimingAngle - desiredAimingAngle) >= 180.f) && (desiredAimingAngle < aimingAngle || (desiredAimingAngle - aimingAngle) < 180.f);
+            if (bNewSwivelDir != bSwivelDir) // if swiveled too much and went past, snap to angle
+            {
+                aimingAngle = desiredAimingAngle;
             }
         }
     }
